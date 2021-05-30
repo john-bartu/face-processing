dbc0a41b61800b8e2e850b4328a633267f1b5ebc
Jamabaiz <bartula.jan@gmail.com>
2021-05-30 13:45:24 +0200

    Testing

---
+++ test/readme.md
@@ -0,0 +1,35 @@
+# example 1
+![](../sample/sample1.png)
+![](1/eye-final.png)
+![](1/hair-final.png)
+![](1/head-final.png)
+
+# example 2
+![](../sample/sample2.png)
+![](2/eye-final.png)
+![](2/hair-final.png)
+![](2/head-final.png)
+
+# example 3
+![](../sample/sample3.png)
+![](3/eye-final.png)
+![](3/hair-final.png)
+![](3/head-final.png)
+
+# example 4
+![](../sample/sample4.png)
+![](4/eye-final.png)
+![](4/hair-final.png)
+![](4/head-final.png)
+
+# example 5
+![](../sample/sample5.png)
+![](5/eye-final.png)
+![](5/hair-final.png)
+![](5/head-final.png)
+
+# example 6
+![](../sample/sample6.png)
+![](6/eye-final.png)
+![](6/hair-final.png)
+![](6/head-final.png)
---
+++ tests.sh
@@ -0,0 +1,11 @@
+py ./texture_generator.py --input_path=sample/sample1.png --input_path_uv=sample/sample1-uv.png --out_dir=test/1/
+
+py ./texture_generator.py --input_path=sample/sample2.png --input_path_uv=sample/sample2-uv.png --out_dir=test/2/
+
+py ./texture_generator.py --input_path=sample/sample3.png --input_path_uv=sample/sample3-uv.png --out_dir=test/3/
+
+py ./texture_generator.py --input_path=sample/sample4.png --input_path_uv=sample/sample4-uv.png --out_dir=test/4/
+
+py ./texture_generator.py --input_path=sample/sample5.png --input_path_uv=sample/sample5-uv.png --out_dir=test/5/
+
+py ./texture_generator.py --input_path=sample/sample6.png --input_path_uv=sample/sample6-uv.png --out_dir=test/6/
